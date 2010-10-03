f2dbb7844b0776b6874f075363309b6ff05e10ea
Adam Monsen <haircut@gmail.com>
2010-10-03 06:03:26 -0700

    WIP: import loan payments Groovy plugin

--- GroovyExample/groovy/examplePlugin.groovy
+++ GroovyExample/groovy/examplePlugin.groovy
@@ -2,7 +2,9 @@ import org.mifos.spi.ParseResultDto
 
 println "Groovy Plugin START."
 
+// TODO: see old AudiBankTsv plugin (deleted in c637ae92bf7762)
 println "input: " + input
+println "first line: " + new BufferedReader(input).readLine()
 println "parent: " + parent
 println "parent.getDisplayName(): " + parent.displayName
 println "parent.getUserReferenceDto(): " + parent.userReferenceDto
